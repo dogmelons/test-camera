b70f5c4045779a3cd5f9b3dffdccab90aaf5ce2e
dogmelons <kyle.easterling@gmail.com>
2013-10-01 20:21:19 -0400

    it works

--- ButterSeal-android/src/edu/smcm/ButterSealGame/Seal.java
+++ ButterSeal-android/src/edu/smcm/ButterSealGame/Seal.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
@@ -27,12 +26,19 @@ public class Seal implements ApplicationListener
 {
 	private int WIDTH;
 	private int HEIGHT;
-	private int cWIDTH = 400;
-	private int cHEIGHT = 240;
+	private int cWIDTH = 300;
+	private int cHEIGHT = 180;
 	private int mWIDTH;
 	private int mHEIGHT;
 	
-	private Texture buzzImage;
+	private int dHEIGHT;
+	private int dWIDTH;
+	private int dHEIGHT3;
+	private int dWIDTH3;
+	
+	private Texture pikaImage;
+	private Texture dpadImage;
+	private Rectangle dpad;
 	private Music DreamMusic;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
@@ -47,13 +53,26 @@ public class Seal implements ApplicationListener
 		
 		Gdx.gl.glClearColor(0f,0,0.0f,1);
 		
-		buzzImage = new Texture(Gdx.files.internal("buzz.png"));
+		pikaImage = new Texture(Gdx.files.internal("pika.png"));
+		dpadImage = new Texture(Gdx.files.internal("dpad.png"));
+		dHEIGHT = dpadImage.getHeight();
+		dWIDTH = dpadImage.getWidth();
+		dHEIGHT3 = dHEIGHT/3;
+		dWIDTH3 = dWIDTH/3;
 		DreamMusic = Gdx.audio.newMusic(Gdx.files.internal("Dream.mp3"));
+		
+		dpad = new Rectangle();
+		dpad.x = 0;
+		dpad.y = 0;
+		dpad.width = dWIDTH;
+		dpad.height = dHEIGHT;
+		
+		batch = new SpriteBatch();
 		
 		touchPos = new Vector3();
 		
-		TiledMap tiledMap = TiledLoader.createMap(Gdx.files.internal("maps/desertTest.tmx"));
-		TileAtlas tileAtlas = new TileAtlas(tiledMap, Gdx.files.internal("maps"));
+		TiledMap tiledMap = TiledLoader.createMap(Gdx.files.internal("collisionmap/collisionTestDesert.tmx"));
+		TileAtlas tileAtlas = new TileAtlas(tiledMap, Gdx.files.internal("collisionmap"));
 		tileMapRenderer = new TileMapRenderer(tiledMap, tileAtlas, 12, 12);
 		
 		mWIDTH = tiledMap.width * tiledMap.tileWidth;
@@ -78,12 +97,13 @@ public class Seal implements ApplicationListener
 	{
 		handleInput();
 
-		
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT );
+		
 		camera.update();
 		tileMapRenderer.render(camera);
-		
-		
+		batch.begin();
+		batch.draw(dpadImage, dpad.x, dpad.y);
+		batch.end();
 	}
 	
 	private void handleInput()
@@ -92,61 +112,72 @@ public class Seal implements ApplicationListener
 		{
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			
-			if((touchPos.x < (WIDTH/2)) && (touchPos.y < (HEIGHT/2))) 	//if screen is touched in top left quadrant
+			//move left
+			if(touchPos.y > (HEIGHT - (dHEIGHT3*2)) && (touchPos.y < (HEIGHT - dHEIGHT3)) && (touchPos.x < dWIDTH3))
 			{
-				if(touchPos.x > touchPos.y) 			//move up
+				if(camera.position.x > (cWIDTH))					//check
+					camera.translate(-3, 0, 0);
+			}
+			//move right
+			else if(touchPos.y > (HEIGHT - (dHEIGHT3*2)) && (touchPos.y < (HEIGHT - dHEIGHT3)) && (touchPos.x > dWIDTH3*2)
+					&& (touchPos.x < dWIDTH))
+			{
+				if(camera.position.x < (mWIDTH-cWIDTH))
+					camera.translate(3, 0, 0);
+			}
+			//move up
+			else if((touchPos.y > (HEIGHT - dHEIGHT)) && (touchPos.y < (HEIGHT - dHEIGHT3*2)) && (touchPos.x > dWIDTH3)
+					&& (touchPos.x < (dWIDTH3*2)))
+			{
+				if(camera.position.y < (mHEIGHT-cHEIGHT))			//out of bounds check
+					camera.translate(0, 3, 0);
+			}
+			//move down
+			else if((touchPos.y > (HEIGHT - dHEIGHT3)) && (touchPos.x > dWIDTH3) && (touchPos.x < (dWIDTH3*2)))
+			{
+				if(camera.position.y > (cHEIGHT))
+					camera.translate(0, -3, 0);	
+			}
+						
+			
+			/*if(touchPos.y < (HEIGHT/2))
+			{		
+				if(touchPos.x < touchPos.y)								//move left
 				{
-					if(camera.position.y < (mHEIGHT-cHEIGHT))		//out of bounds check
-						camera.translate(0, 3, 0);
-				}
-				else 									//move left
-				{
-					if(camera.position.x > (cWIDTH))			//out of bounds check
+					if(camera.position.x > (cWIDTH))					//out of bounds check
 						camera.translate(-3, 0, 0);
 				}
-			}
-			
-			else if(touchPos.x < WIDTH/2) 					//if screen is touched in bottom left quadrant
-			{
-				if(touchPos.x > (HEIGHT - touchPos.y)) 	//move down <------------
-				{	
-					if(camera.position.y > (cHEIGHT))			//check
-						camera.translate(0, -3, 0);
-				}
-				else 									//move left
-				{
-					if(camera.position.x > (cWIDTH))			//check
-						camera.translate(-3, 0, 0);
-				}
-			}
-			
-			else if(touchPos.y < HEIGHT/2) 					//if screen is touched in top right quadrant
-			{
-				if(touchPos.y > (WIDTH - touchPos.x)) 	//move right
+				else if(touchPos.y > (WIDTH - touchPos.x)) 				//move right
 				{
 					if(camera.position.x < (mWIDTH-cWIDTH))
 						camera.translate(3, 0, 0);
 				}
-				else 									//move up <-----------------
+				else													//move up
 				{
-					if(camera.position.y < (mHEIGHT-cHEIGHT))
+					if(camera.position.y < (mHEIGHT-cHEIGHT))			//out of bounds check
 						camera.translate(0, 3, 0);
 				}
 			}
 			
-			else 										//if screen is touched in bottom right quadrant
-			{										
-				if((HEIGHT - touchPos.y) < (WIDTH - touchPos.x)) 			//move down
+			else
+			{
+				if(touchPos.x < (HEIGHT - touchPos.y))					//move left
+				{
+					if(camera.position.x > (cWIDTH))					//check
+						camera.translate(-3, 0, 0);
+				}
+				else if((HEIGHT - touchPos.y) < (WIDTH - touchPos.x))	//move down
 				{
 					if(camera.position.y > (cHEIGHT))
-						camera.translate(0, -3, 0);
+						camera.translate(0, -3, 0);					
 				}
-				else 									//move right <--------------
+				else													//move right
 				{
 					if(camera.position.x < (mWIDTH-cWIDTH))
-					camera.translate(3, 0, 0);
+						camera.translate(3, 0, 0);
 				}
-			}
+			}*/
+			
 		}
 	}
 
